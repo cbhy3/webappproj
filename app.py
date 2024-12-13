b3e980352af3ea9183bcb76b4fadf9e93ceafea8
brian <brianchng07@outlook.com>
2024-12-13 21:10:59 +0800

    added aboutus page

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():  # put application's code here
-    return render_template("home.html")
+def main():
+    return redirect('/aboutus')
+
+@app.route('/aboutus')
+def about_us():
+    return render_template('aboutus.html', active_page='aboutus')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('index.html', active_page='catalog')
 
 
+@app.route('/cart')
+def cart():
+    return render_template('index.html', active_page='cart')
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
