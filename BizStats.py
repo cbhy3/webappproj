ae1dbd48349480c0c898e7c5e619257b3183aaa0
brian <brianchng07@outlook.com>
2025-02-18 16:27:42 +0800

    bug fix

--- BizStats.py
+++ BizStats.py
@@ -26,7 +26,7 @@ def GetStats():
             for x in order.Products:
                 if order.status != "PaymentProcessing":
                     TopSelling.setdefault(order.Products[x][1].name,0)
-                    TopSelling[order.Products[x][1].name][0] += order.Products[x][0]
+                    TopSelling[order.Products[x][1].name] += order.Products[x][0]
 
         temp = dict(sorted(TopSelling.items(), key=lambda item: item[1], reverse=True))       #Top Selling Calculation
         RealTopSelling = dict(list(temp.items())[:10]) #Keep only top 10 selling products
